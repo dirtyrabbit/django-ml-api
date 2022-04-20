c47c023d55bff579c26668fc05ce5bb5434a299e
dirtyrabbit <s8888863@gmail.com>
2022-04-21 00:54:59 +0800

    add predict view

--- Django_ml_api/apps/endpoints/urls.py
+++ Django_ml_api/apps/endpoints/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import include
 from rest_framework.routers import DefaultRouter
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 from apps.endpoints.views import EndpointViewSet
 from apps.endpoints.views import MLAlgorithmViewSet
 from apps.endpoints.views import MLAlgorithmStatusViewSet
 from apps.endpoints.views import MLRequestViewSet
+from apps.endpoints.views import PredictView 
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r"endpoints", EndpointViewSet, basename="endpoints")
@@ -16,5 +17,7 @@ router.register(r"mlrequests", MLRequestViewSet, basename="mlrequests")
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
-
+    re_path(
+        r"^api/v1/(?P<endpoint_name>.+)/predict$", PredictView.as_view(), name="predict"
+    ),
 ]
